39fe58b0ff005a45f632243a98cfe9f4f514b911
Ofgeha-Gelana <ofgehagelana2019@gmail.com>
2025-10-03 07:05:19 +0300

    using with to open file to avoid using finally

---
+++ mastery/6-Exceptions/with_statement_4.py
@@ -0,0 +1,24 @@
+
+# We don't need finally, b/c when we use with it will close automatically
+
+try:
+    with open("app.py") as f:
+        print("File Opened.")
+    age = int(input("Ag: "))
+    xfactor = 10/age
+except (ValueError, ZeroDivisionError, FileNotFoundError):
+    print("You didn't enter a valid age.")
+else:
+    print("No exception was raised.")
+
+
+# When we do with multiple files.
+try:
+    with open("app.py") as f, open("another.py", "w") as target:
+        print("File Opened.")
+    age = int(input("Ag: "))
+    xfactor = 10/age
+except (ValueError, ZeroDivisionError, FileNotFoundError):
+    print("You didn't enter a valid age.")
+else:
+    print("No exception was raised.")
