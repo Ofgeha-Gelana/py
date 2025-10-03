2aae1d9e4beefe6a27121901e2fc9b1adec15610
Ofgeha-Gelana <ofgehagelana2019@gmail.com>
2025-10-03 08:50:54 +0300

    renamed files name

---
+++ mastery/6-Exceptions/cost_of_raising_exc.py
@@ -0,0 +1,15 @@
+from timeit import timeit
+
+code1 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age must be greater than zero.")
+    return 10/age
+
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    print(error)
+"""
+
+print(timeit(code1, ))
