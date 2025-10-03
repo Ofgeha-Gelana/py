30c47c63bc08ac1f0d795266b9d01e5894d42d37
Ofgeha-Gelana <ofgehagelana2019@gmail.com>
2025-10-03 06:48:45 +0300

    ZeroDivisionError included

--- mastery/6-Exceptions/handling_exception2.py
+++ mastery/6-Exceptions/handling_exception2.py
@@ -1,7 +1,21 @@
 
 try:
     age = int(input("Ag: "))
+    xfactor = 10/age
 except ValueError:
     print("You didn't enter a valid age.")
+except ZeroDivisionError:
+    print("Age cannot be 0.")
 else:
     print("No exception was raised.")
+
+try:
+    age = int(input("Ag: "))
+    xfactor = 10 / age
+except (ValueError, ZeroDivisionError):
+    print("You didn't enter a valid age.")
+except ZeroDivisionError:
+    print("Age cannot be 0.")
+else:
+    print("No exception was raised.")
+
