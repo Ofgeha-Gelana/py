435d37373e1188afe46083e697965fd0c7887c3e
Ofgeha-Gelana <ofgehagelana2019@gmail.com>
2025-10-06 13:59:57 +0300

    added python magic methods

--- mastery/class/magic_methods.py
+++ mastery/class/magic_methods.py
@@ -1,11 +1,15 @@
-
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
     def draw(self):
         return f"Point({self.x}, {self.y})"
 
 point = Point(1,2)
-print(point.draw())
+print(point)
 
+point = Point(1,2)
+print(str(point))
