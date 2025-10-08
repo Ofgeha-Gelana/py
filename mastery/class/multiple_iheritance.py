ede4ebb35594d3b6f7b02778dbb75ee5dadc7c6f
Ofgeha-Gelana <ofgehagelana2019@gmail.com>
2025-10-08 12:20:59 +0300

    multiple inheritance

---
+++ mastery/class/multiple_iheritance.py
@@ -0,0 +1,15 @@
+class Employee:
+    def greet(self):
+        print("Employee greet")
+
+
+class Person:
+    def greet(self):
+        print("Person greet")
+
+
+class Manager(Employee, Person):
+    pass
+
+manager = Manager()
+manager.greet()
