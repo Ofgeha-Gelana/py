611063d0a9381922b43860db6bdda80de7151c12
Ofgeha-Gelana <ofgehagelana2019@gmail.com>
2025-10-08 14:29:58 +0300

    added abstract base class

---
+++ mastery/class/abstract_base_class_18.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+
+class InvalidOperationError(Exception):
+    pass
+
+
+class Stream:
+    def __init__(self):
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            raise InvalidOperationError("Stream is already open")
+        self.opened = True
+
+    def close(self):
+        if not self.opened:
+            raise InvalidOperationError("Stream is already closed")
+        self.opened = False
+
+    @abstractmethod
+    def read(self):
+        pass
+
+class FileStream(Stream):
+    def read(self):
+        print("Reading data from file")
+
+class NetworkStream(Stream):
+    def read(self):
+        print("Reading data from network")
+
+class MemoryStream(Stream):
+    def read(self):
+        print("Reading data from memory")
+
+
+stream = Stream()
+stream.open()
